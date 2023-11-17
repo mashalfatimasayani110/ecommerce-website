fd56ac49b4cee83f28506a6eb9a698d400889480
Mashal Fatima Sayani <49507663+mashalfatimasayani110@users.noreply.github.com>
2023-11-17 19:46:06 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Navbar
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar)
+{
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close)
+{
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+// Shop Page
+var mainImg = document.getElementById("main");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function(){
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    mainImg.src = smallImg[3].src;
+}
